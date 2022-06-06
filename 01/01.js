ab4e611b1c89186526227f0eadf3563cd305119f
juanitaNJ <juanitanarez@gmail.com>
2022-06-06 16:28:47 -0500

    01

--- 01/01.js
+++ 01/01.js
@@ -10,6 +10,8 @@ function filterArray(arr) {
     return filteredArray;
 }
 
+// function should filter out the strings and keep the numbers in the array that's passed in
+
 console.log(filterArray([1, 2, "a", "b"]));
 console.log(filterArray([1, "a", "b", 0, 15]));
 console.log(filterArray([1, 2, "aasf", "1", "123", 123]));
